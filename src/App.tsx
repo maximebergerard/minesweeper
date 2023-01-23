ea129ae86696e6a5d1b5959277d3504632ad9fd7
maximebergerard <maxime.bergerard@gmail.com>
2023-01-23 14:51:16 +0100

    ✨ Add bombs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from "react";
-import "./App.css";
+import React from "react"
+import "./App.css"
 
-const gridWidth = 25;
-const gridHeight = 15;
+const gridWidth = 25
+const gridHeight = 15
 
-const board = Array(gridHeight)
+const board: number[][] = Array(gridHeight)
   .fill(0)
-  .map(() => new Array(gridWidth).fill(0));
-console.log(board);
+  .map(() => new Array(gridWidth).fill(0))
+console.log(board)
+
+function getRandomArbitrary(min: number, max: number) {
+  min = Math.floor(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+// let bombNumber = 40
+const makeBombs = (bombsCount: number) => {
+  const randomA = getRandomArbitrary(0, gridWidth)
+  const randomB = getRandomArbitrary(0, gridHeight)
+
+  if (bombsCount === 0) {
+    return board
+  } else if (board[randomB][randomA] === 0) {
+    board[randomB][randomA] = 1
+    makeBombs(bombsCount - 1)
+  } else {
+    makeBombs(bombsCount)
+  }
+}
+
+makeBombs(40)
 
 const App = () => {
   return (
@@ -20,17 +43,17 @@ const App = () => {
                 {items.map((subItems, subIndex) => {
                   return (
                     <td key={subIndex}>
-                      <div className="cell" />
+                      <div className={`cell cell${subItems}`} />
                     </td>
-                  );
+                  )
                 })}
               </tr>
-            );
+            )
           })}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
